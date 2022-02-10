8004d0cb55bd44b81f7acf7270241a918f6de25d
Lavliet90 <60854775+Lavliet90@users.noreply.github.com>
2022-02-10 19:32:27 +0300

    'v.0.2'

--- Geometria/Geom/geometry/views.py
+++ Geometria/Geom/geometry/views.py
@@ -1,13 +1,29 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
+from django.shortcuts import render
 
-def get_rectangle_area(request, wigth: int, height: int):
-    return HttpResponse(f'The area of the rectangle {wigth} x {height} equals {int(wigth * height)}')
+
+def get_rectangle_area(request, width: int, height: int):
+    data = {
+        'the_size_x': int(width),
+        'the_size_y': int(height),
+        'equals': int(width * height),
+    }
+    return render(request, 'geometry/rectangle.html', context=data)
 
 
 def get_square_area(request, width: int):
-    return HttpResponse(f'The area of the square {width} x {width} equals {int(width * width)}')
+    data = {
+        'the_size': int(width),
+        'equals': int(width ** 2),
+    }
+    return render(request, 'geometry/square.html', context=data)
 
 
-def get_circle_area(request, radius: float):
-    return HttpResponse(f'Area of a circle with a radius {radius} equals {float(radius * radius * 3.14)}')
+def get_circle_area(request, radius: int):
+    data = {
+        'radius': int(radius),
+        'equals': float(radius ** 2 * 3.141),
+    }
+    return render(request, 'geometry/circle.html', context=data)
